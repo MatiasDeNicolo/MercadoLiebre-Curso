67b724163841a174bf2703e41edd0699c6bac111
MatiasDeNicolo <matiasdenicolo@hotmail.com>
2021-10-11 22:15:49 -0300

    nuevo actualizacion

--- app.js
+++ app.js
@@ -9,10 +9,15 @@ app.use(express.static(publicPath));
 
 
 
-app.listen(3000, () => {
+//app.listen(3000, () => {
+ //   console.log("Servidor corriendo en puerto 3000");
+//}
+//);
+
+app.listen(process.env.PORT) || 3000, function () {
     console.log("Servidor corriendo en puerto 3000");
 }
-);
+
 
 app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/home.html"))
@@ -33,6 +38,8 @@ app.post("/register", (req, res) => {
 })
 
 app.use(express.urlencoded());
+
+
 
 
 // esto va para el pryecto integrador.//
